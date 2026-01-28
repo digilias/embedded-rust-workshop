daf5e30333e83c5e05ee4f23ba55b3dd15a8ee50
Ulf Lilleengen <ulf@digili.no>
2026-01-28 13:21:33 +0100

    start with non-async

--- practice/workshop/src/main.rs
+++ practice/workshop/src/main.rs
@@ -1,26 +1,29 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
-use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+#[cortex_m_rt::entry]
+fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    defmt::info!("Hello World!");
 
     let mut led = Output::new(p.PB0, Level::High, Speed::Low);
 
     loop {
-        info!("high");
+        defmt::info!("high");
         led.set_high();
-        Timer::after_millis(500).await;
 
-        info!("low");
+        for _i in 0..1000000{
+            cortex_m::asm::nop();
+        }
+
+        defmt::info!("low");
         led.set_low();
-        Timer::after_millis(500).await;
+
+        for _i in 0..1000000{
+            cortex_m::asm::nop();
+        }
     }
 }
